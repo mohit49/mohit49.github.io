f923330c027be0d058ed6258c9383885d49ec5ae
Mohit Sharma <sharmamohit@adobe.com>
2023-08-12 12:13:46 +0530

    Updates

---
+++ precache-manifest.59d8820d1f484d5936363a2828b6b741.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "48a0ba2299456507a9b7",
+    "url": "/static/css/main.5123b3d0.chunk.css"
+  },
+  {
+    "revision": "48a0ba2299456507a9b7",
+    "url": "/static/js/main.48a0ba22.chunk.js"
+  },
+  {
+    "revision": "f881440d46e811047be5",
+    "url": "/static/js/1.f881440d.chunk.js"
+  },
+  {
+    "revision": "d848b5e0faae72e653af",
+    "url": "/static/js/2.d848b5e0.chunk.js"
+  },
+  {
+    "revision": "33b20e4610c15ddc4db9",
+    "url": "/static/js/runtime~main.33b20e46.js"
+  },
+  {
+    "revision": "865cbcc880b4fb4741424a2a9cbb9e58",
+    "url": "/static/media/home.865cbcc8.jpg"
+  },
+  {
+    "revision": "c4b617e16122be35e4f3c96e5f508e86",
+    "url": "/static/media/work.c4b617e1.png"
+  },
+  {
+    "revision": "7df6367eaafa53cef111ff176056487a",
+    "url": "/static/media/cover.7df6367e.jpg"
+  },
+  {
+    "revision": "07d345c3e38c5b776d7f442852a8cb24",
+    "url": "/static/media/img1.07d345c3.png"
+  },
+  {
+    "revision": "4e6ec2629df5cb57be20f9bf55937938",
+    "url": "/static/media/sniptolldev.4e6ec262.png"
+  },
+  {
+    "revision": "327fd3d13d0018bb766c36177ab37aec",
+    "url": "/static/media/slider.327fd3d1.png"
+  },
+  {
+    "revision": "d706429788e19ae63f42268b6477215d",
+    "url": "/static/media/westcon.d7064297.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6fe06026d9419f8462f084e222bb4a5e",
+    "url": "/index.html"
+  }
+];
